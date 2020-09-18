b96b2f0874808f7690ea7d6ccd6db49385dce5d5
biswal19 <chandrikabiswal1@gmail.com>
2020-09-18 05:42:17 -0700

    Updates

---
+++ precache-manifest.11a86d608c8e87d6832d788cc93f8539.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1becbefdb3b5d7c87ce015cbc253cad",
+    "url": "/react-meals-app/index.html"
+  },
+  {
+    "revision": "d702956eb29dfc7c28b2",
+    "url": "/react-meals-app/static/css/2.5c0dc3ac.chunk.css"
+  },
+  {
+    "revision": "e5008b0defbb617ba310",
+    "url": "/react-meals-app/static/css/main.0afe985b.chunk.css"
+  },
+  {
+    "revision": "d702956eb29dfc7c28b2",
+    "url": "/react-meals-app/static/js/2.22b5f314.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-meals-app/static/js/2.22b5f314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5008b0defbb617ba310",
+    "url": "/react-meals-app/static/js/main.153d0fb5.chunk.js"
+  },
+  {
+    "revision": "d4e267fed36c6a7fb283",
+    "url": "/react-meals-app/static/js/runtime-main.fe96d913.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-meals-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-meals-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-meals-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-meals-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-meals-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
